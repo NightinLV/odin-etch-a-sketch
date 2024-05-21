00082f930adfd4c7bf86512d7c139c9c4737edd8
NightinLV <laurvrincianu@gmail.com>
2024-05-21 17:37:30 +0200

    Create drawing effect on mouse hover, add prompt for user to change board size

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+const userInput = prompt("Choose your board size between 1 and 100:", "16");
+
+
 const boardContainer = document.querySelector(".container");
 
-let size = 16 * 16;
+let size = userInput * userInput;
 
 function createBoard() {
     for (let i = 0; i < size; i+= 1) {
         const boardSquare = document.createElement("div");
         boardSquare.className = "board-square"
-        boardSquare.style.minWidth = 960 / 16 + "px";
+        boardSquare.style.minWidth = 960 / userInput + "px";
         boardSquare.style.minHeight = boardSquare.style.width;
         boardContainer.appendChild(boardSquare);
     }
 }
 
+function drawing() {
+    const boardSquares = document.querySelectorAll(".container > div");
+    
+    boardSquares.forEach((square) => {
+        const boardSquare = square;
+        boardSquare.addEventListener("mouseenter", (event) => {
+            event.target.style.backgroundColor = "black";
+        });
+    });
+}
+
 createBoard();
+drawing();
